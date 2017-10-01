dafbce6eff88dc48e9cda6c932720b6679ae1102
Alireza <A.Sattari@outlook.com>
2017-09-30 23:26:05 -0700

    Better version

--- Firebase_Side.py
+++ Firebase_Side.py
@@ -6,26 +6,54 @@ from firebase import firebase
 firebase = firebase.FirebaseApplication('https://group-a1.firebaseio.com')
 #url = 'https://group-a1.firebaseio.com/username'
 
-# firebase.post('/account', {'password' : 'Its data', 'mastery' : 0, 'point' : 0, 'notes' : 0, 'questions' : 0,
-#                               'answers' : 0, 'friends' : 0})
+# firebase.post('/account', {'username' : 'data', 'password' : 'Its data555', 'mastery' : 0, 'point' : 0, 'notes' : 0, 'questions' : 0,
+#                                'answers' : 0, 'friends' : 0})
 
+
+def get_key(username):
+    tmp = '/' + username
+    userData = firebase.get(tmp, None)
+    if userData == None:
+        return None
+    key = next(iter(userData))
+    return key
+
+def get_info(username, key):
+    tmp = '/'+username
+    userData = firebase.get(tmp, None)
+    if userData == None:
+        return None
+    key = get_key(username)
+    info = userData[key]
+    return info
 
 def get_account(username, password):
-    userData = firebase.get('/account', None)
-
-    key = next(iter(userData))
-    sDictionary = userData[key]
-    [sDictionary['password']]
-
+    userData = get_info()   # dictionary of account info
+    ret = None
     if userData != None:
-        ret = []
-        ret.append(userData["mastery"])
-        ret.append(userData["points"])
-        ret.append(userData["notes"])
-        ret.append(userData["questions"])
-        ret.append(userData["answer"])
-        ret.append(userData["friends"])
-    else:
-        ret = None
+        if password == userData["password"]:
+            ret = []
+            ret.append(userData["mastery"])
+            ret.append(userData["points"])
+            ret.append(userData["notes"])
+            ret.append(userData["questions"])
+            ret.append(userData["answer"])
+            ret.append(userData["friends"])
 
     return ret
+
+def delete_account(username):
+    key = get_key(username)
+    
+
+    return
+
+def write_account():
+    return
+
+def update_account():
+    return
+
+
+
+get_account("a", "b")
