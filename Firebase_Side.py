b124c6a149daab323338c9f9c3f6350af754773c
Alireza <A.Sattari@outlook.com>
2017-09-30 22:34:58 -0700

    Somehow working

--- Firebase_Side.py
+++ Firebase_Side.py
@@ -10,25 +10,22 @@ firebase = firebase.FirebaseApplication('https://group-a1.firebaseio.com')
 #                               'answers' : 0, 'friends' : 0})
 
 
-userData = firebase.get('/account', None)
-
-test = userData['-KvKbRIXG0rsGQ0ryyIb']
-test2=[test['password']]
-print (test2)
-
 def get_account(username, password):
-	userData = firebase.get('/account', None)
-	print (userData)
+    userData = firebase.get('/account', None)
 
-	if userData != None:
-	    ret = []
-	    ret.append(userData["mastery"])
-	    ret.append(userData["points"])
-	    ret.append(userData["notes"])
-	    ret.append(userData["questions"])
-	    ret.append(userData["answer"])
-	    ret.append(userData["friends"])
-	else:
-	    ret = None
+    key = next(iter(userData))
+    sDictionary = userData[key]
+    [sDictionary['password']]
 
-	return ret
+    if userData != None:
+        ret = []
+        ret.append(userData["mastery"])
+        ret.append(userData["points"])
+        ret.append(userData["notes"])
+        ret.append(userData["questions"])
+        ret.append(userData["answer"])
+        ret.append(userData["friends"])
+    else:
+        ret = None
+
+    return ret
