b63584854962095fb8cda14172a3dd86e4176df9
Alireza <A.Sattari@outlook.com>
2017-10-01 00:06:39 -0700

    Deleting function added

--- Firebase_Side.py
+++ Firebase_Side.py
@@ -4,23 +4,17 @@
 
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://group-a1.firebaseio.com')
-#url = 'https://group-a1.firebaseio.com/username'
-
-# firebase.post('/account', {'username' : 'data', 'password' : 'Its data555', 'mastery' : 0, 'point' : 0, 'notes' : 0, 'questions' : 0,
-#                                'answers' : 0, 'friends' : 0})
 
 
 def get_key(username):
-    tmp = '/' + username
-    userData = firebase.get(tmp, None)
+    userData = firebase.get(username, None)
     if userData == None:
         return None
     key = next(iter(userData))
     return key
 
-def get_info(username, key):
-    tmp = '/'+username
-    userData = firebase.get(tmp, None)
+def get_info(username):
+    userData = firebase.get(username, None)
     if userData == None:
         return None
     key = get_key(username)
@@ -28,32 +22,39 @@ def get_info(username, key):
     return info
 
 def get_account(username, password):
-    userData = get_info()   # dictionary of account info
+    userData = get_info(username)   # dictionary of account info
     ret = None
     if userData != None:
         if password == userData["password"]:
             ret = []
             ret.append(userData["mastery"])
-            ret.append(userData["points"])
+            ret.append(userData["point"])
             ret.append(userData["notes"])
             ret.append(userData["questions"])
-            ret.append(userData["answer"])
+            ret.append(userData["answers"])
             ret.append(userData["friends"])
 
     return ret
 
 def delete_account(username):
-    key = get_key(username)
-    
-
+    firebase.delete(username, None)
     return
 
-def write_account():
+def write_account(username,password,mastery, points, notes, questions, answers, friends):
+    firebase.post(username, {'password': password, 'mastery': mastery, 'points': points,
+                             'notes': notes, 'questions': 0, 'answers' : answers, 'friends' : friends})
     return
 
 def update_account():
     return
 
 
-
-get_account("a", "b")
+username = "account"
+username = "/" + username
+password = "a"
+account = get_account(username, password)
+if account != None:
+    delete_account(username)
+    print("Successful Delete")
+else:
+    print("Account does not exist")
